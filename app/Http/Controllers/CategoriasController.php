bf97c3d60841d56843d0994739453aa46132081a
Jeferson0512 <jeferson.bujaico@tecsup.edu.pe>
2018-06-05 20:38:37 -0500

    bien

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -13,13 +13,8 @@ class CategoriasController extends Controller
         $categoria = Categorias::all();
         return $categoria;
     }
-<<<<<<< HEAD
-    public function show_length(){
-        $categoria = Categoria::count()->get();
-=======
     public function longitud(){
         $categoria = Categorias::count();
->>>>>>> 857aa81643130c119f86ed945f305e49440900f7
         return $categoria;
     }
 }
